57b1bfd78a90a2453bfb756367e0447e70884450
gbotemi <gbotemibolarinwa@gmail.com>
2020-08-12 00:25:27 +0100

    remove element solved

--- removeElement.py
+++ removeElement.py
@@ -1,7 +1,9 @@
-def removeElement(nums,val):
-    for i in range(len(nums)-1):
-        if nums[i] == val:
-            nums.remove(nums[i])
-    return len(nums)
-    
-    
+class Solution:
+    def removeElement(self, nums: List[int], val: int) -> int:
+        x=nums.count(val)
+        y = len(nums)
+        a=[]
+        temp=0
+        for i in range(x):
+            nums.remove(val)
+        
