46f62432f4b3ab3b71538f679576e7a8ad674a99
gbotemi <gbotemibolarinwa@gmail.com>
2020-08-19 02:54:49 +0100

    a faster implementation, but less memory

--- replaceElements.py
+++ replaceElements.py
@@ -1,10 +1,20 @@
-def num(arr):
-    for i in range(len(arr)):
-        
-        if i == len(arr)-1:
-            arr[i] = -1
-            return arr
-        length = i + 1
-        maxx = max(arr[length:])
-        arr[i]= maxx
-    return arr
+class Solution:
+    def replaceElements(self, arr: List[int]) -> List[int]:
+        for i in range(len(arr)-1,-1,-1):
+            
+            if i == (len(arr)-1):
+                temp1 = arr[i]
+                arr[i] = -1
+                
+            elif i == (len(arr)-2):
+                temp2 = arr[i]
+                arr[i] = temp1
+                if temp2 < temp1:
+                    temp2 = temp1
+                
+            else:
+                temp3 = arr[i]
+                arr[i] = temp2
+                if temp2 < temp3:
+                    temp2= temp3
+        return arr
